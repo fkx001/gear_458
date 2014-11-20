683b759ccddd76af6f4983d9b40b79b4abbb7796
Kangxing Fu <kangxingf@gmail.com>
2014-11-19 21:57:43 -0800

    Initial Commit

---
+++ gear_458/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gear_458
+//
+//  Created by Kangxing Fu on 11/19/14.
+//  Copyright (c) 2014 Kangxing Fu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
